7b3ecbf4639baa6a095735c581df0f2d7a2b5046
Emily Gagne <ceg9498@gmail.com>
2022-10-13 15:51:01 -0400

    Initial port from Codepen.

---
+++ logic.js
@@ -0,0 +1,799 @@
+let kanji = [
+	{
+	 kanji: "日",
+	 meaning: "day",
+	 onyomi: "ニチ、 ジツ",
+	 kunyomi: "ひ、 -び、 -か"
+	},
+	{
+	 kanji: "月",
+	 meaning: "moon",
+	 onyomi: "ゲツ、 ガツ",
+	 kunyomi: "つき"
+	},
+	{
+	 kanji: "火",
+	 meaning: "fire",
+	 onyomi: "カ",
+	 kunyomi: "ひ、 -び、 ほ-"
+	},
+	{
+	 kanji: "水",
+	 meaning: "water",
+	 onyomi: "スイ",
+	 kunyomi: "みず、 みず-"
+	},
+	{
+	 kanji: "木",
+	 meaning: "tree, wood",
+	 onyomi: "ボク、 モク",
+	 kunyomi: "き、 こ-"
+	},
+	{
+	 kanji: "金",
+	 meaning: "gold, money",
+	 onyomi: "キン、 コン、 ゴン",
+	 kunyomi: "かね、 かな-、 -がね"
+	},
+	{
+	 kanji: "土",
+	 meaning: "earth, soil",
+	 onyomi: "ド、 ト",
+	 kunyomi: "つち"
+	},
+	{
+	 kanji: "一",
+	 meaning: "one",
+	 onyomi: "イチ、 イツ",
+	 kunyomi: "ひと"
+	},
+	{
+	 kanji: "ニ",
+	 meaning: "two",
+	 onyomi: "ニ、ジ",
+	 kunyomi: "ふた"
+	},
+	{
+	 kanji: "三",
+	 meaning: "three",
+	 onyomi: "サン",
+	 kunyomi: "み"
+	},
+	{
+	 kanji: "四",
+	 meaning: "four",
+	 onyomi: "シ",
+	 kunyomi: "よ、よん"
+	},
+	{
+	 kanji: "五",
+	 meaning: "five",
+	 onyomi: "ゴ",
+	 kunyomi: "いつ"
+	},
+	{
+	 kanji: "六",
+	 meaning: "six",
+	 onyomi: "ロク、リク",
+	 kunyomi: "む、むい"
+	},
+	{
+	 kanji: "七",
+	 meaning: "seven",
+	 onyomi: "シチ",
+	 kunyomi: "なな、なの"
+	},
+	{
+	 kanji: "八",
+	 meaning: "eight",
+	 onyomi: "ハチ",
+	 kunyomi: "や"
+	},
+	{
+	 kanji: "九",
+	 meaning: "nine",
+	 onyomi: "キュウ、ク",
+	 kunyomi: "ここの"
+	},
+	{
+	 kanji: "十",
+	 meaning: "ten",
+	 onyomi: "ジュウ、ジツ",
+	 kunyomi: "とお、と"
+	},
+	{
+	 kanji: "百",
+	 meaning: "hundred",
+	 onyomi: "ヒャク、ビャク",
+	 kunyomi: "もも"
+	},
+	{
+	 kanji: "千",
+	 meaning: "thousand",
+	 onyomi: "セン",
+	 kunyomi: "ち"
+	},
+	{
+	 kanji: "万",
+	 meaning: "ten thousand",
+	 onyomi: "マン、バン",
+	 kunyomi: "よろず"
+	},
+	{
+	 kanji: "億",
+	 meaning: "one hundred million",
+	 onyomi: "オク",
+	 kunyomi: ""
+	},
+	{
+	 kanji: "円",
+	 meaning: "Yen, circle, round",
+	 onyomi: "エン",
+	 kunyomi: "まるい、まどか"
+	},
+	{
+	 kanji: "年",
+	 meaning: "year",
+	 onyomi: "ネン",
+	 kunyomi: "とし"
+	},
+	{
+	 kanji: "朝",
+	 meaning: "morning",
+	 onyomi: "チョウ",
+	 kunyomi: "あさ"
+	},
+	{
+	 kanji: "昼",
+	 meaning: "noon",
+	 onyomi: "チュウ",
+	 kunyomi: "ひる"
+	},
+	{
+	 kanji: "晩",
+	 meaning: "evening",
+	 onyomi: "バン",
+	 kunyomi: ""
+	},
+	{
+	 kanji: "時",
+	 meaning: "time, hour",
+	 onyomi: "ジ",
+	 kunyomi: "とき、どき"
+	},
+	{
+	 kanji: "週",
+	 meaning: "week",
+	 onyomi: "シュウ",
+	 kunyomi: ""
+	},
+	{
+	 kanji: "後",
+	 meaning: "behind, later",
+	 onyomi: "ゴ、コウ",
+	 kunyomi: "のち、うしろ"
+	},
+	{
+	 kanji: "前",
+	 meaning: "in front, before",
+	 onyomi: "ゼン",
+	 kunyomi: "まえ"
+	},
+	{
+	 kanji: "人",
+	 meaning: "person",
+	 onyomi: "ジン、 ニン",
+	 kunyomi: "ひと"
+	},
+	{
+	 kanji: "犬",
+	 meaning: "dog",
+	 onyomi: "ケン",
+	 kunyomi: "いぬ"
+	},
+	{
+	 kanji: "猫",
+	 meaning: "cat",
+	 onyomi: "ビョウ",
+	 kunyomi: "ねこ"
+	},
+	{
+	 kanji: "私",
+	 meaning: "I, me",
+	 onyomi: "シ",
+	 kunyomi: "わたし、わたくし"
+	},
+	{
+	 kanji: "門",
+	 meaning: "gate",
+	 onyomi: "モン",
+	 kunyomi: "かど、 と"
+	},
+	{
+	 kanji: "本",
+	 meaning: "book; volume; script",
+	 onyomi: "ホン",
+	 kunyomi: "もと"
+	},
+	{
+	 kanji: "麺",
+	 meaning: "noodles",
+	 onyomi: "メン、 ベン",
+	 kunyomi: "むぎこ"
+	},
+	{
+	 kanji: "牛",
+	 meaning: "cow, beef",
+	 onyomi: "ギュウ",
+	 kunyomi: "うし"
+	},
+	{
+	 kanji: "魚",
+	 meaning: "fish",
+	 onyomi: "ギョ",
+	 kunyomi: "うお、 さかな"
+	},
+	{
+	 kanji: "豚",
+	 meaning: "pig",
+	 onyomi: "トン",
+	 kunyomi: "ぶた"
+	},
+	{
+	 kanji: "鳥",
+	 meaning: "bird",
+	 onyomi: "チョウ",
+	 kunyomi: "とり"
+	},
+	{
+	 kanji: "家",
+	 meaning: "house, family",
+	 onyomi: "",
+	 kunyomi: "いえ、 や、 うち"
+	},
+	{
+	 kanji: "町",
+	 meaning: "town",
+	 onyomi: "チョウ",
+	 kunyomi: "まち"
+	},
+	{
+	 kanji: "国",
+	 meaning: "country, nation",
+	 onyomi: "コク",
+	 kunyomi: "くに"
+	},
+	{
+	 kanji: "物",
+	 meaning: "thing",
+	 onyomi: "ブツ、 モツ",
+	 kunyomi: "もの"
+	},
+	{
+	 kanji: "見",
+	 meaning: "see",
+	 onyomi: "ケン",
+	 kunyomi: "み"
+	},
+	{
+	 kanji: "行",
+	 meaning: "go",
+	 onyomi: "コウ、 ギョウ、 アン",
+	 kunyomi: "い"
+	},
+	{
+	 kanji: "帰",
+	 meaning: "return",
+	 onyomi: "キ",
+	 kunyomi: "かえ、おく、 とつ"
+	},
+	{
+	 kanji: "話",
+	 meaning: "talk",
+	 onyomi: "ワ",
+	 kunyomi: "はな"
+	},
+	{
+	 kanji: "読",
+	 meaning: "read",
+	 onyomi: "ドク、 トク、 トウ",
+	 kunyomi: "よ"
+	},
+	{
+	 kanji: "書",
+	 meaning: "write",
+	 onyomi: "ショ",
+	 kunyomi: "かく、がき"
+	},
+	{
+	 kanji: "食",
+	 meaning: "eat, food",
+	 onyomi: "ショク、 ジキ",
+	 kunyomi: "くう、くらう、た"
+	},
+	{
+	 kanji: "飲",
+	 meaning: "drink",
+	 onyomi: "イン、 オン",
+	 kunyomi: "の"
+	},
+	{
+	 kanji: "会",
+	 meaning: "meet, meeting, association",
+	 onyomi: "カイ、 エ",
+	 kunyomi: "あ"
+	},
+	{
+	 kanji: "待",
+	 meaning: "wait",
+	 onyomi: "タイ",
+	 kunyomi: "ま"
+	},
+	{
+	 kanji: "買",
+	 meaning: "buy",
+	 onyomi: "バイ",
+	 kunyomi: "か"
+	},
+	{
+	 kanji: "出",
+	 meaning: "exit, leave",
+	 onyomi: "シュツ、 スイ",
+	 kunyomi: "で、だ、い"
+	},
+	{
+	 kanji: "使",
+	 meaning: "use, order",
+	 onyomi: "シ",
+	 kunyomi: "つか、 -づか"
+	},
+	{
+	 kanji: "教",
+	 meaning: "teach",
+	 onyomi: "キョウ",
+	 kunyomi: "おし.える、 おそ.わる"
+	},
+	{
+	 kanji: "忘",
+	 meaning: "forget",
+	 onyomi: "ボウ",
+	 kunyomi: "わす"
+	},
+	{
+	 kanji: "大",
+	 meaning: "big",
+	 onyomi: "だい",
+	 kunyomi: "おお"
+	},
+	{
+	 kanji: "小",
+	 meaning: "small",
+	 onyomi: "しょう",
+	 kunyomi: "ちい、こ"
+	},
+	{
+	 kanji: "次",
+	 meaning: "next",
+	 onyomi: "ジ",
+	 kunyomi: "つぎ"
+	},
+	{
+	 kanji: "毎",
+	 meaning: "every",
+	 onyomi: "マイ",
+	 kunyomi: "ごと"
+	},
+	{
+	 kanji: "間",
+	 meaning: "between",
+	 onyomi: "カン",
+	 kunyomi: "あいだ"
+	},
+	{
+	 kanji: "隣",
+	 meaning: "next to, neighbour",
+	 onyomi: "リン",
+	 kunyomi: "となり"
+	},
+	{
+	 kanji: "上",
+	 meaning: "on top of",
+	 onyomi: "ジョウ",
+	 kunyomi: "うえ、うわ"
+	},
+	{
+	 kanji: "下",
+	 meaning: "under",
+	 onyomi: "ゲ",
+	 kunyomi: "した"
+	},
+	{
+	 kanji: "中",
+	 meaning: "inside, middle",
+	 onyomi: "チュウ",
+	 kunyomi: "なか"
+	},
+	{
+	 kanji: "右",
+	 meaning: "right",
+	 onyomi: "ウ",
+	 kunyomi: "みぎ"
+	},
+	{
+	 kanji: "左",
+	 meaning: "left",
+	 onyomi: "サ",
+	 kunyomi: "ひだり"
+	},
+	{
+	 kanji: "北",
+	 meaning: "north",
+	 onyomi: "ホク",
+	 kunyomi: "きた"
+	},
+	{
+	 kanji: "南",
+	 meaning: "south",
+	 onyomi: "ナン",
+	 kunyomi: "みなみ"
+	},
+	{
+	 kanji: "東",
+	 meaning: "east",
+	 onyomi: "トウ",
+	 kunyomi: "ひがし"
+	},
+	{
+	 kanji: "西",
+	 meaning: "west",
+	 onyomi: "サイ",
+	 kunyomi: "にし"
+	},
+ ];
+ let hiragana = [
+	{
+	 item: "あ"
+	},
+	{
+	 item: "い"
+	},
+	{
+	 item: "う"
+	},
+	{
+	 item: "え"
+	},
+	{
+	 item: "お"
+	},
+	{
+	 item: "か"
+	},
+	{
+	 item: "き"
+	},
+	{
+	 item: "く"
+	},
+	{
+	 item: "け"
+	},
+	{
+	 item: "こ"
+	},
+	{
+	 item: "さ"
+	},
+	{
+	 item: "し"
+	},
+	{
+	 item: "す"
+	},
+	{
+	 item: "せ"
+	},
+	{
+	 item: "そ"
+	},
+	{
+	 item: "た"
+	},
+	{
+	 item: "ち"
+	},
+	{
+	 item: "つ"
+	},
+	{
+	 item: "て"
+	},
+	{
+	 item: "と"
+	},
+	{
+	 item: "な"
+	},
+	{
+	 item: "に"
+	},
+	{
+	 item: "ぬ"
+	},
+	{
+	 item: "ね"
+	},
+	{
+	 item: "の"
+	},
+	{
+	 item: "は"
+	},
+	{
+	 item: "ひ"
+	},
+	{
+	 item: "ふ"
+	},
+	{
+	 item: "へ"
+	},
+	{
+	 item: "ほ"
+	},
+	{
+	 item: "ま"
+	},
+	{
+	 item: "み"
+	},
+	{
+	 item: "む"
+	},
+	{
+	 item: "め"
+	},
+	{
+	 item: "も"
+	},
+	{
+	 item: "や"
+	},
+	{
+	 item: "ゆ"
+	},
+	{
+	 item: "よ"
+	},
+	{
+	 item: "ら"
+	},
+	{
+	 item: "り"
+	},
+	{
+	 item: "る"
+	},
+	{
+	 item: "れ"
+	},
+	{
+	 item: "ろ"
+	},
+	{
+	 item: "わ"
+	},
+	{
+	 item: "を"
+	},
+	{
+	 item: "ん"
+	},
+ ];
+ let katakana = [
+	{
+	 item: "ア"
+	},
+	{
+	 item: "イ"
+	},
+	{
+	 item: "ウ"
+	},
+	{
+	 item: "エ"
+	},
+	{
+	 item: "お"
+	},
+	{
+	 item: "か"
+	},
+	{
+	 item: "キ"
+	},
+	{
+	 item: "ク"
+	},
+ {
+	 item: "ケ"
+	},
+	{
+	 item: "コ"
+	},
+	{
+	 item: "サ"
+	},
+	{
+	 item: "シ"
+	},
+	{
+	 item: "ス"
+	},
+	{
+	 item: "セ"
+	},
+	{
+	 item: "ソ"
+	},
+	{
+	 item: "タ"
+	},
+	{
+	 item: "チ"
+	},
+	{
+	 item: "ツ"
+	},
+	{
+	 item: "テ"
+	},
+	{
+	 item: "ト"
+	},
+	{
+	 item: "ナ"
+	},
+	{
+	 item: "ニ"
+	},
+	{
+	 item: "ヌ"
+	},
+	{
+	 item: "ネ"
+	},
+	{
+	 item: "ノ"
+	},
+	{
+	 item: "ハ"
+	},
+	{
+	 item: "ヒ"
+	},
+	{
+	 item: "フ"
+	},
+	{
+	 item: "ヘ"
+	},
+	{
+	 item: "ホ"
+	},
+	{
+	 item: "マ"
+	},
+	{
+	 item: "ミ"
+	},
+	{
+	 item: "ム"
+	},
+	{
+	 item: "メ"
+	},
+	{
+	 item: "モ"
+	},
+	{
+	 item: "ヤ"
+	},
+	{
+	 item: "ユ"
+	},
+	{
+	 item: "ヨ"
+	},
+	{
+	 item: "ラ"
+	},
+	{
+	 item: "リ"
+	},
+	{
+	 item: "ル"
+	},
+	{
+	 item: "レ"
+	},
+	{
+	 item: "ロ"
+	},
+	{
+	 item: "ワ"
+	},
+	{
+	 item: "ヲ"
+	},
+	{
+	 item: "ン"
+	},
+ ];
+
+window.onload = () => {
+	let bingoToken = document.getElementById("bingo-token");
+	bingoToken.innerHTML = "<span class='english'>Hi</span>";
+
+	let bingoInfo = document.getElementById("bingo-token-info");
+	bingoInfo.innerHTML = "<span class='info english'>Select a character set, then</span><span class='info english'>click 'New Game' start a game.</span>";
+
+	document.getElementById("new-game").onclick = newGame;
+	document.getElementById("refresh").onclick = getItem;
+
+	let items, current, type;
+
+	function newGame() {
+		type = document.getElementById("character-set").value;
+		if(type == "kanji"){
+			items = shuffle(kanji);
+		} else if(type == "hiragana"){
+			items = shuffle(hiragana);
+		} else {
+			items = shuffle(katakana);
+		}
+		current = 0;
+	}
+
+	function shuffle(arr){
+		let count = arr.length;
+		let index, temp;
+
+		while(count > 0){
+			index = Math.floor(Math.random()*count);
+			count--;
+
+			temp = arr[count];
+			arr[count] = arr[index];
+			arr[index] = temp;
+		}
+		return arr;
+	}
+
+	function getItem() {
+		if(current == items.length) {
+			// end of array
+			document.getElementById("bingo-token").innerHTML = "";
+			document.getElementById("bingo-token-info").innerHTML = "<span class='info'>End of data!</span>";
+			newGame();
+			return;
+		}
+		current++;
+		if(type == "kanji"){
+			displayKanji(items[current]);
+		}
+		if(type == "hiragana" || type == "katakana") {
+			displayKana(items[current]);
+		}
+	}
+
+	function displayKanji(item){
+		document.getElementById("bingo-token").innerHTML = item.kanji;
+		document.getElementById("bingo-token-info").innerHTML = "<span class='info english'>Meaning: " + item.meaning + "</span><span class='info'>訓読み: " + item.kunyomi + "</span><span class='info'>音読み: " + item.onyomi + "</span>";
+	}
+
+	function displayKana(item){
+		document.getElementById("bingo-token").innerHTML = item.item;
+		document.getElementById("bingo-token-info").innerHTML = item.info ? item.info : "";
+	}
+}
